28d0c5e1746ee6acc5f0a67f677af3b764b41621
Ali Talaat <43627253+alifcis2019@users.noreply.github.com>
2024-04-20 14:23:27 +0200

    testing

---
+++ content/js/general.js
@@ -0,0 +1,21 @@
+
+/**
+ * Preloader
+ */
+const preloader = document.querySelector("#preloader");
+if (preloader) {
+    document.querySelector("body").style.overflow = "hidden";
+    window.addEventListener("load", () => {
+        document.querySelector("body").style.overflow = "auto";
+        preloader.remove();
+    });
+}
+
+
+
+
+// initialize aos library
+AOS.init();
+
+
+
